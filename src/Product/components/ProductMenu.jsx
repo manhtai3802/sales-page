a36f94aab6d9312c9919f494c1130bee166e50d0
manhtai3802 <fbmanhtai@gmail.com>
2023-09-30 00:10:06 +0700

    update product menu

--- src/Product/components/ProductMenu.jsx
+++ src/Product/components/ProductMenu.jsx
@@ -1,27 +1,8 @@
 import { Box, Tab, Tabs } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { useState } from 'react';
 import { Link, Navigate, Route, Routes, useParams } from 'react-router-dom';
 
 ProductMenu.propTypes = {};
-
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    flexFlow: 'row nowrap',
-    listStyleType: 'none',
-    justifyContent: 'space-evenly',
-    padding: 0,
-
-    '& > li': {
-      padding: '16px 24px',
-    },
-
-    '& > li > a.active': {
-      color: 'grey',
-      textDecoration: 'underline',
-    },
-  },
-}));
 
 const PageOne = () => <h2>PageOne</h2>;
 const PageTwo = () => <h2>PageTwo</h2>;
@@ -29,14 +10,17 @@ const PageThree = () => <h2>PageThree</h2>;
 const PageFour = () => <h2>PageFour</h2>;
 function ProductMenu(props) {
   const { productId } = useParams();
-  const classes = useStyles();
-
   const pathname = `/products/${productId}`;
+  const [value, setValue] = useState(`${pathname}/PageOne`);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Box>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs aria-label="Tab navigation" value={`${pathname}/PageOne`} variant="fullWidth">
-          <Tab label="Tab One" component={Link} to={`${pathname}/`} value={`${pathname}/PageOne`} />
+        <Tabs aria-label="Tab navigation" value={value} variant="fullWidth" onChange={handleChange}>
+          <Tab label="Tab One" component={Link} to={`${pathname}/PageOne`} value={`${pathname}/PageOne`} />
           <Tab label="Tab Two" component={Link} to={`${pathname}/pageTwo`} value={`${pathname}/pageTwo`} />
           <Tab label="Tab Three" component={Link} to={`${pathname}/pageThree`} value={`${pathname}/pageThree`} />
           <Tab label="Tab Four" component={Link} to={`${pathname}/pageFour`} value={`${pathname}/pageFour`} />
@@ -44,10 +28,10 @@ function ProductMenu(props) {
       </Box>
       <Routes>
         <Route index element={<Navigate to={`${pathname}/PageOne`} />} />
-        <Route path={`${pathname}/PageOne`} element={<PageOne />} />
-        <Route path={`${pathname}/pageTwo`} element={<PageTwo />} />
-        <Route path={`${pathname}/pageThree`} element={<PageThree />} />
-        <Route path={`${pathname}/pageFour`} element={<PageFour />} />
+        <Route path={`/PageOne`} element={<PageOne />} />
+        <Route path={`/pageTwo`} element={<PageTwo />} />
+        <Route path={`/pageThree`} element={<PageThree />} />
+        <Route path={`/pageFour`} element={<PageFour />} />
       </Routes>
     </Box>
   );
