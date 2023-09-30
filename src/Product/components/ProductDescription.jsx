927514b2a674a026d4a37ae65fc826f7756c1448
manhtai3802 <fbmanhtai@gmail.com>
2023-09-30 09:52:47 +0700

    update route product menu

---
+++ src/Product/components/ProductDescription.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ProductDescription.propTypes = {};
+
+function ProductDescription(props) {
+  return <div>ProductDescription</div>;
+}
+
+export default ProductDescription;
--- src/Product/components/ProductMenu.jsx
+++ src/Product/components/ProductMenu.jsx
@@ -1,37 +1,37 @@
 import { Box, Tab, Tabs } from '@mui/material';
 import { useState } from 'react';
-import { Link, Navigate, Route, Routes, useParams } from 'react-router-dom';
+import { Link, Navigate, Route, Routes, useLocation, useParams } from 'react-router-dom';
+import ProductAdditional from './ProductAdditional';
+import ProductDescription from './ProductDescription';
+import ProductReview from './ProductReview';
 
-ProductMenu.propTypes = {};
-
-const PageOne = () => <h2>PageOne</h2>;
-const PageTwo = () => <h2>PageTwo</h2>;
-const PageThree = () => <h2>PageThree</h2>;
-const PageFour = () => <h2>PageFour</h2>;
 function ProductMenu(props) {
   const { productId } = useParams();
+  const pathnameReset = useLocation().pathname;
   const pathname = `/products/${productId}`;
-  const [value, setValue] = useState(`${pathname}/PageOne`);
-
+  const [value, setValue] = useState(pathnameReset !== null ? pathnameReset : `${pathname}/description`);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   return (
     <Box>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs aria-label="Tab navigation" value={value} variant="fullWidth" onChange={handleChange}>
-          <Tab label="Tab One" component={Link} to={`${pathname}/PageOne`} value={`${pathname}/PageOne`} />
-          <Tab label="Tab Two" component={Link} to={`${pathname}/pageTwo`} value={`${pathname}/pageTwo`} />
-          <Tab label="Tab Three" component={Link} to={`${pathname}/pageThree`} value={`${pathname}/pageThree`} />
-          <Tab label="Tab Four" component={Link} to={`${pathname}/pageFour`} value={`${pathname}/pageFour`} />
+        <Tabs
+          aria-label="Tab navigation"
+          value={value === pathname ? `${pathname}/description` : value}
+          variant="fullWidth"
+          onChange={handleChange}
+        >
+          <Tab label="Description" component={Link} to={`${pathname}/description`} value={`${pathname}/description`} />
+          <Tab label="Additional" component={Link} to={`${pathname}/additional`} value={`${pathname}/additional`} />
+          <Tab label="Review" component={Link} to={`${pathname}/review`} value={`${pathname}/review`} />
         </Tabs>
       </Box>
       <Routes>
-        <Route index element={<Navigate to={`${pathname}/PageOne`} />} />
-        <Route path={`/PageOne`} element={<PageOne />} />
-        <Route path={`/pageTwo`} element={<PageTwo />} />
-        <Route path={`/pageThree`} element={<PageThree />} />
-        <Route path={`/pageFour`} element={<PageFour />} />
+        <Route index element={<Navigate to={`${pathname}/description`} />} />
+        <Route path={`/description`} element={<ProductDescription />} />
+        <Route path={`/additional`} element={<ProductAdditional />} />
+        <Route path={`/review`} element={<ProductReview />} />
       </Routes>
     </Box>
   );
